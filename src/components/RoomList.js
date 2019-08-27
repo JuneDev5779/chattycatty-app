97542477477cfa036ce3e99878bde78b3c3bdbbc
Junibel De La Cruz <junibeldelacruz@gmail.com>
2019-08-26 20:08:51 -0600

    can create new rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,9 +5,13 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: '',
     };
+
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
@@ -18,13 +22,43 @@ class RoomList extends Component {
     });
   }
 
+  handleUserInput(e){
+    this.setState({
+      newRoomName: e.target.value
+    });
+    console.log(this.state.newRoomName);
+  }
+
+  createRoom() {
+    if(this.state.newRoomName.trim() !== ""){
+      this.roomsRef.push({
+        name: this.state.newRoomName
+      });
+      this.setState({
+        newRoomName: '',
+      });
+    }
+    else{
+      alert("Please enter a Room Name");
+      this.setState({
+        newRoomName: '',
+      });
+    }
+  }
+
+
   render() {
     return (
       <section>
-      <h1>Chatty Catty Chat Rooms</h1>
+      <h1>Welcome to Chatty Catty</h1>
+      <ul>
       {this.state.rooms.map( room =>
-        <li>{room.name}</li>
+        <li key={room.key}>{room.name}</li>
       )}
+      </ul>
+
+      <input type="text" placeholder="Name the new chat room..." value={this.state.newRoomName} onChange={this.handleUserInput}/>
+      <button onClick={this.createRoom}>Create New Catty Chatty Chat Room</button>
 
       </section>
     );
